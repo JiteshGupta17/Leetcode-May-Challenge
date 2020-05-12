adca94e17a8575de93461f340d7afd8f61495557
JiteshGupta17 <54665900+JiteshGupta17@users.noreply.github.com>
2020-05-12 13:23:49 +0530

    Single Element in a Sorted

---
+++ 12 May.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int singleNonDuplicate(vector<int>& nums) {
+        
+        int l = 0, r = nums.size()-1;
+        
+        while(l < r) {
+        
+            int mid = l + (r-l)/2;
+            
+            if(mid%2==0) {
+            
+                if(nums[mid]==nums[mid+1]) l=mid+2;
+                else r=mid;
+            }
+            else {
+            
+                if(nums[mid]==nums[mid-1]) l=mid+1;
+                else r=mid-1;
+            }
+        }
+        
+        return nums[l];
+    }
+};
